e8d396953ac67aeaf5dd5d7c49598a3cb87818ae
Shubhgupta-coder <95521631+Shubhgupta-coder@users.noreply.github.com>
2024-08-02 21:06:11 +0530

    DB SETUP

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// DATABASE KO USE KRTE SAMYA TRY CATCH OR ASYNC AWAIT KA USE KRNA CHIAIYE
+// this line tells us jitni zldi hamari app load ho utni zldi hamre envirnment variable load ho jaaye
+// require('dotenv').config({path:'./env'});  //object tells us ki hamari home directory k andar hi env h
+// import mongoose from 'mongoose';
+// import { DB_NAME } from './constants.js';
+
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
+dotenv.config(
+    {
+        path:'./env'
+    }
+)
+connectDB();
+// here we used IIIFE   
+
+// connect database genneral syntax
+// mongoose.connect('mongodb://127.0.0.1:27017/test');  (URL/DTAABASE NAME)
+// Approach 1 to connect DB
+/*
+import express from "express";
+const app = express()
+;(async()=>{
+    try {
+           await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        //    sometime in professional work we use listener to catch error
+        app.on("error",(error)=>{
+            console.log("ERROR:",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`APP IS LISTEN ON portt ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+})()    
+    */
